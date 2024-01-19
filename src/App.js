2c8ac8395f105ca4b7dedf8722a2793dfcd413cf
enduraew <enduraew@gmail.com>
2024-01-19 23:02:36 +0500

    upd

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <div className="App">
             <h1 style={{fontSize: 70, color: 'red'}}>value = {state}</h1>
             <button
-                style={{background: 'black', color: "white", padding: 15}}
+                style={{background: 'white', color: "black", padding: 15}}
                 onClick={increment}
             >
                 inc by +1
